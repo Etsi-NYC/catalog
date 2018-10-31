4c1ea550b0c3d6906783db21adc97e6edd03db88
grantdiamond <grantadiamond@gmail.com>
2018-10-31 10:41:26 -0400

    query functions for fetching data from mysql

---
+++ db/index.js
@@ -0,0 +1,58 @@
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    user: 'root',
+    password: '',
+    database: 'etsi'
+  }
+});
+
+var getFirstItem = function(itemNum, cb) {
+  knex
+    .select()
+    .from('items')
+    .where('item_id', '=', Number(itemNum))
+    .then(row => {
+      if (row.length === 0) {
+        cb('error');
+      }
+      cb(row);
+    });
+};
+
+var getSeller = function(id, cb) {
+  knex
+    .select()
+    .from('sellers')
+    .where('seller_id', '=', Number(id))
+    .then(seller => {
+      cb(seller);
+    });
+};
+
+var getSimilarItems = function(itemNum, cb) {
+  knex
+    .select('item_category', 'seller_id')
+    .from('items')
+    .where('item_id', '=', itemNum)
+    .then(results => {
+      let sellerId = results[0].seller_id;
+      let category = results[0].item_category;
+      return knex
+        .select()
+        .from('items')
+        .where('seller_id', '=', sellerId)
+        .andWhere('item_category', '=', category)
+        .andWhere('free_shipping', '=', 1)
+        .orWhere('item_category', '<>', category)
+        .orWhere('free_shipping', '=', 0)
+        .limit(9);
+    })
+    .then(rows => {
+      cb(rows);
+    });
+};
+
+exports.getFirstItem = getFirstItem;
+exports.getSeller = getSeller;
+exports.getSimilarItems = getSimilarItems;
